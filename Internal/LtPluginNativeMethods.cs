49c63143acce6480a9d438165da74781b6a3a283
Scott Michaud <scott@lighttwist.io>
2022-02-24 15:17:03 -0500

    Cleanup native methods and switch back to dynamic loading.

--- Internal/LtPluginNativeMethods.cs
+++ Internal/LtPluginNativeMethods.cs
@@ -7,8 +7,7 @@ using UnityEngine;
 public static class LtPluginNativeMethods
 {
     
-    #if UNITY_EDITOR_OSX && false
-
+#if UNITY_EDITOR_OSX
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     internal delegate IntPtr get_shared_texture_t(int tex_id, int[] dimensions); //dimensions: width, height
     internal static get_shared_texture_t get_shared_texture;
@@ -65,82 +64,35 @@ public static class LtPluginNativeMethods
     
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     internal delegate void studio_load_callback_t(IntPtr utf8URL);
-#endif
-
-#if (UNITY_STANDALONE_OSX && !UNITY_EDITOR_OSX) || true
-    [DllImport("libLTUnityBridge")]
-    internal static extern IntPtr get_shared_texture(int tex_id, int[] dimensions);
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern IntPtr set_shared_texture(IntPtr texPtr, int id);
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern IntPtr texture_updated(int id);
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern int teardown_plugin();
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern int init_plugin();
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern int setup_webcam_port();
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern int clear_webcam_port();
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern void get_scaling_parameters(float[] scaling_parameters);
-    
-    [DllImport("libLTUnityBridge")]
-    internal static extern void register_keyboard_events_callback(key_callback_t callback);
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern void register_shared_texture_changed_callback(shared_texture_changed_callback_t callback);
-
-    [DllImport("libLTUnityBridge")]
-    internal static extern void register_studio_load_callback(studio_load_callback_t callback);
-    
-    [DllImport("libLTUnityBridge")]
-    internal static extern ELtRequestUpdateError request_update(byte[] buffer, int bufferSize);
-    
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    internal delegate void key_callback_t(KeyCode keycode, ELtKeycodeState state);
-
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    internal delegate void shared_texture_changed_callback_t(IntPtr tex_ref, int tex_id, int width, int height);
-    
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    internal delegate void studio_load_callback_t(IntPtr utf8URL);
-#endif
 
     [DllImport("libLTUnityBridge", CharSet = CharSet.Ansi)]
     internal static extern void write_to_log(string str);
+#endif
 
     internal static void RegisterSwiftMethods()
     {
-    // #if UNITY_EDITOR_OSX
-    //     var libpath = get_unique_library_instance(interface_lib);
-    //     get_shared_texture = FunctionLoader.LoadFunction<get_shared_texture_t>(libpath, "get_shared_texture");
-    //     set_shared_texture = FunctionLoader.LoadFunction<set_shared_texture_t>(libpath, "set_shared_texture");
-    //     texture_updated = FunctionLoader.LoadFunction<texture_updated_t>(libpath, "texture_updated");
-    //     teardown_plugin = FunctionLoader.LoadFunction<teardown_plugin_t>(libpath, "teardown_plugin");
-    //     init_plugin = FunctionLoader.LoadFunction<init_plugin_t>(libpath, "init_plugin");
-    //     setup_webcam_port = FunctionLoader.LoadFunction<setup_webcam_mach_port_t>(libpath, "setup_webcam_port");
-    //     clear_webcam_uint = FunctionLoader.LoadFunction<clear_webcam_port_uint_t>(libpath, "clear_webcam_port_uint");
-    //     //update_plugin = FunctionLoader.LoadFunction<update_plugin_t>(libpath, "update_plugin");
-    //     get_scaling_parameters =
-    //         FunctionLoader.LoadFunction<get_scaling_parameters_t>(libpath, "get_scaling_parameters");
-    //     register_keyboard_events_callback =
-    //         FunctionLoader.LoadFunction<register_keyboard_events_callback_t>(libpath,
-    //             "register_keyboard_events_callback");
-    //     register_shared_texture_changed_callback =
-    //         FunctionLoader.LoadFunction<register_shared_texture_changed_callback_t>(libpath,
-    //             "register_shared_texture_changed_callback");
-    //     register_studio_load_callback =
-    //         FunctionLoader.LoadFunction<register_studio_load_callback_t>(libpath, "register_studio_load_callback");
-    //     request_update = FunctionLoader.LoadFunction<request_update_t>(libpath, "request_update");
-    // #endif
+#if UNITY_EDITOR_OSX
+        var libpath = get_unique_library_instance(interface_lib);
+        get_shared_texture = FunctionLoader.LoadFunction<get_shared_texture_t>(libpath, "get_shared_texture");
+        set_shared_texture = FunctionLoader.LoadFunction<set_shared_texture_t>(libpath, "set_shared_texture");
+        texture_updated = FunctionLoader.LoadFunction<texture_updated_t>(libpath, "texture_updated");
+        teardown_plugin = FunctionLoader.LoadFunction<teardown_plugin_t>(libpath, "teardown_plugin");
+        init_plugin = FunctionLoader.LoadFunction<init_plugin_t>(libpath, "init_plugin");
+        setup_webcam_port = FunctionLoader.LoadFunction<setup_webcam_mach_port_t>(libpath, "setup_webcam_port");
+        clear_webcam_uint = FunctionLoader.LoadFunction<clear_webcam_port_uint_t>(libpath, "clear_webcam_port_uint");
+        //update_plugin = FunctionLoader.LoadFunction<update_plugin_t>(libpath, "update_plugin");
+        get_scaling_parameters =
+            FunctionLoader.LoadFunction<get_scaling_parameters_t>(libpath, "get_scaling_parameters");
+        register_keyboard_events_callback =
+            FunctionLoader.LoadFunction<register_keyboard_events_callback_t>(libpath,
+                "register_keyboard_events_callback");
+        register_shared_texture_changed_callback =
+            FunctionLoader.LoadFunction<register_shared_texture_changed_callback_t>(libpath,
+                "register_shared_texture_changed_callback");
+        register_studio_load_callback =
+            FunctionLoader.LoadFunction<register_studio_load_callback_t>(libpath, "register_studio_load_callback");
+        request_update = FunctionLoader.LoadFunction<request_update_t>(libpath, "request_update");
+#endif
     }
     
     #if !UNITY_EDITOR_OSX
@@ -151,9 +103,8 @@ public static class LtPluginNativeMethods
     #endif
     
     #if UNITY_EDITOR_OSX
-    ////////////////////////////////////////////
-    // below here it's just dylib plumbing
-    const string interface_lib = "Assets/NativeLibrary/libLTUnityBridge.dylib";
+
+    const string interface_lib = "Packages/com.lighttwist.lighttwistunitysdk/Internal/NativeLibrary/libLTUnityBridge.dylib";
 
     public static void OnApplicationQuit()
     {
@@ -169,7 +120,7 @@ public static class LtPluginNativeMethods
         FunctionLoader.FreeLibraries();
     }
 
-    static List<string> temp_libs = new List<string>();
+    static readonly List<string> temp_libs = new();
     static string get_unique_library_instance(string orig_path)
     {
         var unique_path = Path.GetTempFileName() + "_" + Path.GetFileName(orig_path);
@@ -180,7 +131,6 @@ public static class LtPluginNativeMethods
         temp_libs.Add(unique_path);
 
         return unique_path;
-    // make sure we clean up later
     }
 
     class FunctionLoader
@@ -194,7 +144,7 @@ public static class LtPluginNativeMethods
         }
 
     // tracking what we loaded for easier cleanup
-        static List<IntPtr> handles = new List<IntPtr>();
+        static readonly List<IntPtr> handles = new();
 
         public static void FreeLibraries()
         {
