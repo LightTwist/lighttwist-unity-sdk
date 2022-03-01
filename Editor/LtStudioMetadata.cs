0fb4e0f506202e320f43f6dce703a6644817b342
Scott Michaud <scott@lighttwist.io>
2022-03-01 00:30:24 -0500

    Import SDK from interior 3.

---
+++ Editor/LtStudioMetadata.cs
@@ -0,0 +1,186 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using System.IO.Compression;
+using UnityEditor.SceneManagement;
+using Debug = UnityEngine.Debug;
+
+public class LtStudioMetadata : EditorWindow
+{
+    private string assetBundlePath = "";
+    
+    private string jsonStudioVersion = "1.0";
+    private string jsonStudioName = "";
+    private string jsonStudioId = "";
+
+    private string studioFilePrefix = "";
+
+    [MenuItem("Window/LightTwist/Package Asset Bundle Into Studio")]
+    private static void ShowWindow()
+    {
+        var window = GetWindow<LtStudioMetadata>();
+        window.titleContent = new GUIContent("Package LtStudio");
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        const string tempFolderName = "LtStudioCreate";
+        const string thumbnailName = "thumbnail.jpg";
+        
+        GUILayout.Label("Packaging AssetBundle as Studio", EditorStyles.boldLabel);
+        
+        EditorGUILayout.Space(16);
+        if (string.IsNullOrWhiteSpace(assetBundlePath))
+        {
+            GUILayout.Label("Please select an asset bundle");
+        }
+        else
+        {
+            GUILayout.Label(assetBundlePath, EditorStyles.wordWrappedLabel);
+        }
+
+        if (GUILayout.Button("Select Asset Bundle"))
+        {
+            assetBundlePath = EditorUtility.OpenFilePanel("Select Asset Bundle", "", "");
+        }
+        EditorGUILayout.Space(10);
+        jsonStudioVersion = EditorGUILayout.TextField("(Metadata) Studio Version", jsonStudioVersion);
+        jsonStudioName = EditorGUILayout.TextField("(Metadata) Studio Display Name", jsonStudioName);
+        jsonStudioId = EditorGUILayout.TextField("(Metadata) Studio ID", jsonStudioId);
+
+        studioFilePrefix = EditorGUILayout.TextField("Studio File Name Prefix", studioFilePrefix);
+
+        EditorGUILayout.Space(20);
+        if (GUILayout.Button("Validate Studio"))
+        {
+            if (string.IsNullOrWhiteSpace(assetBundlePath))
+            {
+                throw new Exception("Asset Bundle Path is not set");
+            }
+
+            if (File.Exists(assetBundlePath) == false)
+            {
+                throw new Exception("Asset Bundle Path does not lead to a file");
+            }
+            
+            if (File.Exists(Path.Combine(tempFolderName, thumbnailName)) == false)
+            {
+                throw new Exception($"Thumbnail does not exist. Please place a {thumbnailName} in a folder called {tempFolderName} that's in your project folder (as a sibling of Assets, Library, Packages, ProjectSettings, etc.).");
+            }
+
+            var currentRootObjects = EditorSceneManager.GetActiveScene().GetRootGameObjects();
+
+            List<LtMainCamera> mainCameras = new();
+            
+            foreach (var currentRoot in currentRootObjects)
+            {
+                mainCameras.AddRange(currentRoot.GetComponents<LtMainCamera>());
+            }
+
+            if (mainCameras.Count < 1)
+            {
+                throw new Exception("The world needs at least one main camera");
+            }
+
+            if (mainCameras.Count > 1)
+            {
+                throw new Exception("The world cannot have more than one main camera");
+            }
+            
+            Debug.Log("Validation complete. Studio is valid.");
+        }
+        
+        if (GUILayout.Button("Build Studio"))
+        {
+            var tempFolderPath = Path.Combine(Path.GetTempPath(), tempFolderName);
+            var studioFolderName = $"{studioFilePrefix}.ltstudio";
+            var studioFolderPath = Path.Combine(tempFolderPath, studioFolderName);
+            CreateOrEmptyDirectory(tempFolderPath);
+            Directory.CreateDirectory(studioFolderPath);
+
+            const string metadataJsonName = "metadata.json";
+            const string studioFileName = "studio-unity";
+            
+            // Fill folder with relevant files
+            File.Copy(assetBundlePath, Path.Combine(studioFolderPath, studioFileName));
+            File.Copy(Path.Combine(tempFolderName, thumbnailName), Path.Combine(studioFolderPath, thumbnailName));
+            CreateSettingsFile(Path.Combine(studioFolderPath, metadataJsonName));
+
+            var outputZip = $"{studioFolderName}.zip";
+            if (File.Exists(outputZip))
+            {
+                File.Delete(outputZip);
+            }
+            
+            // Zip folder into asset bundle
+            //ZipFile.CreateFromDirectory(tempFolderPath, outputZip);
+            CreateZipFromFolder(tempFolderPath, outputZip);
+            File.Copy(Path.Combine(tempFolderPath, outputZip), outputZip);
+        }
+    }
+
+    private static void CreateOrEmptyDirectory(string argDesiredPath)
+    {
+        if (Directory.Exists(argDesiredPath))
+        {
+            Directory.Delete(argDesiredPath, true);
+        }
+
+        Directory.CreateDirectory(argDesiredPath);
+    }
+
+    private void CreateSettingsFile(string argPath)
+    {
+        var lines = new[]
+        {
+            "{",
+            $"   \"version\": \"{jsonStudioVersion}\",",
+            $"   \"studio_name\": \"{jsonStudioName}\",",
+            $"   \"studio_id\": \"{jsonStudioId}\"",
+            "}"
+        };
+        
+        File.WriteAllLines(argPath, lines);
+    }
+
+    private static void CreateZipFromFolder(string argFolderToZip, string argZipToCreate)
+    {
+#if UNITY_EDITOR_WIN
+        CreateZipFromFolderWindowsEditor(argFolderToZip, argZipToCreate);
+#else
+        CreateZipFromFolderPosix(argFolderToZip, argZipToCreate);
+#endif
+    }
+
+    private static void CreateZipFromFolderWindowsEditor(string argFolderToZip, string argZipToCreate)
+    {
+        //string binPath = Path.Combine(Application.dataPath, "Editor", "third-party", "7zip", "windows");
+        string binPath = "Packages/com.lighttwist.lighttwistunitysdk/Editor/third-party/7zip/windows";
+        var processInfo = new ProcessStartInfo
+        {
+            FileName = Path.Combine(binPath, "7za.exe"),
+            WorkingDirectory = argFolderToZip,
+            Arguments = $"a {argZipToCreate} *"
+        };
+
+        using var zipProcess = Process.Start(processInfo);
+        zipProcess?.WaitForExit();
+    }
+
+    private static void CreateZipFromFolderPosix(string argFolderToZip, string argZipToCreate)
+    {
+        var processInfo = new ProcessStartInfo
+        {
+            FileName = "zip",
+            WorkingDirectory = argFolderToZip,
+            Arguments = $"-r {argZipToCreate} ."
+        };
+
+        using var zipProcess = Process.Start(processInfo);
+        zipProcess?.WaitForExit();
+    }
+}
